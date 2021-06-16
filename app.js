05fc9de207cf6910e80c31f36990bed25cfbf8bc
f1shy-dev <agarwal.vrishank@gmail.com>
2021-06-16 17:54:15 +0000

    push pog

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-const express = require('express');
-const helmet = require('helmet');
-const { ErrorResponseObject } = require('./common/http');
-const routes = require('./routes');
+const app = require("express")();
+const fetch = require("node-fetch");
 
-const app = express();
+const config = {
+  port: 3000,
+  baseURL: "http://old.reddit.com",
+};
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(helmet());
-app.use('/', routes);
+app.use("*", function (req, res) {
+  console.log(req.get("host"));
+  var date = new Date().toISOString().substr(11, 8);
+  var url = config.baseURL + req.originalUrl;
+  logger(date, req.ip, url);
+  fetch(url, {
+    method: req.method,
+    headers: { "User-Agent": req.headers["user-agent"] },
+  })
+    .then((res) => res.blob())
+    .then((body) => {
+      res.type(body.type);
+      body.arrayBuffer().then((buf) => {
+        res.send(Buffer.from(buf));
+      });
+    });
+});
 
-// default catch all handler
-app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
+function logger(date, ip, url) {
+  if (url.length > 60) {
+    console.log(`[-] [${date}] ${ip} => ${url.substr(0, 60)}...`);
+  } else {
+    console.log(`[-] [${date}] ${ip} => ${url}`);
+  }
+}
 
 module.exports = app;
+
+// app.listen(config.port, () => {
+//   console.clear();
+//   console.log(`[!] Proxy Started at http://localhost:${config.port}`);
+// });

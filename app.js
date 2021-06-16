6c9e97fdeb8a76bbf845c21bc7a125da9fec6ee3
f1shy-dev <agarwal.vrishank@gmail.com>
2021-06-16 18:13:16 +0000

    protocol

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const fetch = require("node-fetch");
 
 const config = {
   port: 3000,
-  baseURL: "http://old.reddit.com",
+  baseURL: "https://old.reddit.com",
   host: "f1shproxy",
   otherHost: process.env.VERCEL_URL.split(".")[0].replace(/_/g, "."),
 };
 
 app.use("*", function (req, res) {
-  const remoteHost = req.get("host").split(".")[0].replace(/_/g, ".");
+  const remoteHost =
+    req.protocol + "://" + req.get("host").split(".")[0].replace(/_/g, ".");
   let url = config.baseURL;
   if (remoteHost != config.host && remoteHost != config.otherHost) {
     url = remoteHost + req.originalUrl;

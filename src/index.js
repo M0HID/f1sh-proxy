712072deb490bb49755f8ce3f7194a6f4fc78356
f1shy-dev <agarwal.vrishank@gmail.com>
2022-01-28 21:25:58 +0000

    fix: godamm try catch block

--- src/index.js
+++ src/index.js
@@ -21,47 +21,53 @@ let { hosts, resolvers } = {
 process.env.VERCEL && hosts.push(process.env.VERCEL_URL);
 
 app.use("*", (req, res) => {
-  const host = req.get("host"); // f1shproxy.ml or fp-12.vercel.app
-  const remote = req.get("host").split(".")[0]; // google_com or fp-12 or f1shproxy
-  const parsedRemote = parseURL(remote); // google.com
-  // const { protocol } = req; // path after host such as /waffle?q=21
-  const protocol = "https";
-  const remoteHref = `${protocol}://${parsedRemote}`;
+  try {
+    const host = req.get("host"); // f1shproxy.ml or fp-12.vercel.app
+    const remote = req.get("host").split(".")[0]; // google_com or fp-12 or f1shproxy
+    const parsedRemote = parseURL(remote); // google.com
+    // const { protocol } = req; // path after host such as /waffle?q=21
+    const protocol = "https";
+    const remoteHref = `${protocol}://${parsedRemote}`;
 
-  if (hosts.includes(host) || host.split(".").length == 2)
-    return resolvers.browser(req, res);
+    if (hosts.includes(host) || host.split(".").length == 2)
+      return resolvers.browser(req, res);
 
-  if (resolvers[parsedRemote]) return resolvers[parsedRemote](req, res);
+    if (resolvers[parsedRemote]) return resolvers[parsedRemote](req, res);
 
-  const fixedOrigin = remote.endsWith("_or");
+    const fixedOrigin = remote.endsWith("_or");
 
-  const ogURL = fixedOrigin
-    ? req.originalUrl.split("/").slice(1).join("/")
-    : req.originalUrl;
+    const ogURL = fixedOrigin
+      ? req.originalUrl.split("/").slice(1).join("/")
+      : req.originalUrl;
 
-  let headers = cleanReqHeaders(req.headers);
+    let headers = cleanReqHeaders(req.headers);
 
-  headers.origin &&
-    (headers.origin = fixedOrigin ? parseURL(ogURL.split("/")[0]) : remoteHref);
-  headers.referer && (headers.referer = `${remoteHref}${ogURL}`);
-  headers.host = parsedRemote;
+    headers.origin &&
+      (headers.origin = fixedOrigin
+        ? parseURL(ogURL.split("/")[0])
+        : remoteHref);
+    headers.referer && (headers.referer = `${remoteHref}${ogURL}`);
+    headers.host = parsedRemote;
 
-  log(`${remoteHref}${ogURL}`, "remoteURL");
-  log(host, "host");
-  log(remote, "remote");
-  log(parsedRemote, "parsedRemote");
-  log(protocol, "protocol");
-  log(ogURL, "ogURL");
-  log(remoteHref, "remoteHref");
-  log(fixedOrigin, "fixedOrigin");
-  log(headers, "sending headers");
+    log(`${remoteHref}${ogURL}`, "remoteURL");
+    log(host, "host");
+    log(remote, "remote");
+    log(parsedRemote, "parsedRemote");
+    log(protocol, "protocol");
+    log(ogURL, "ogURL");
+    log(remoteHref, "remoteHref");
+    log(fixedOrigin, "fixedOrigin");
+    log(headers, "sending headers");
 
-  return followFetch(`${remoteHref}${ogURL}`, res, {
-    method: req.method,
-    headers,
-    body: req.body || null,
-    redirect: "manual",
-  });
+    return followFetch(`${remoteHref}${ogURL}`, res, {
+      method: req.method,
+      headers,
+      body: req.body || null,
+      redirect: "manual",
+    });
+  } catch (e) {
+    res.send(`error: ${e.messsage}\n\n${e.stack}`);
+  }
 });
 
 const followFetch = async (url, res, config) => {

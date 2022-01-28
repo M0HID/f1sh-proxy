3289d25a1f6d8894420a00e0ddfc7061619fef96
f1shy-dev <agarwal.vrishank@gmail.com>
2022-01-28 19:41:14 +0000

    fix: fix prefix bug

--- src/util/logging.js
+++ src/util/logging.js
@@ -1,6 +1,6 @@
 const str = (i) => JSON.stringify(i);
-const log = (msg, prefix) => {
-  const prefix = `[${prefix}] ` || "";
+const log = (msg, pf) => {
+  const prefix = `[${pf}] ` || "";
   if (typeof i == "object") console.log(`${prefix}${str(i)}`);
   else console.log(`${prefix}${msg}`);
 };

0de972fb66f361ac8ec75cf5b4ece19b02f9302b
f1shy-dev <agarwal.vrishank@gmail.com>
2022-01-28 13:08:18 +0000

    v2

--- src/index.js
+++ src/index.js
@@ -1,101 +1,47 @@
-const l = (p, l) =>
-  console.log(`[${p}] ${typeof l === "object" ? JSON.stringify(l) : l}`);
-
 const { join } = require("path");
 const app = require("express")();
 const fetch = require("node-fetch");
-const rewriteHTML = require("./util/rewriteHTML");
-const config = {
-  port: 3000,
-  host: "f1shproxy",
-  rewrite: {
-    // default url to send all <>.fp.ml requests to
-    default: "browser.f1shproxy.ml",
-    // base url
-    normal: "f1shproxy.ml",
-    // origin rewrite url
-    origin: `origin_rewrite.f1shproxy.ml`,
-  },
-  hosts: {
-    base: "f1shproxy",
-    vercel: process.env.VERCEL
-      ? process.env.VERCEL_URL.split(".")[0].replace(/_/g, ".")
-      : "",
-  },
-};
 
 app.use(require("cors")());
 
+const { hosts, resolvers } = {
+  hosts: ["f1shproxy.ml"],
+  resolvers: {
+    default: proxyURL,
+    browser: (res) => res.sendFile(join(__dirname, "index.html")),
+  },
+};
+
+const parseURL = (u) => u.replace(/_/g, ".");
+
 app.use("*", (req, res) => {
-  const { rewrite, hosts } = config;
-  // apple_com.f1shproxy.ml => apple.com
-  const remote = req.get("host").split(".")[0].replace(/_/g, ".");
-  let url = rewrite.default;
+  const host = req.get("host").split(".").reverse()[1]; // f1shproxy.ml
+  const remote = req.get("host").split(".")[0]; //google_com
+  const parsedRemote = parseURL(remote);
+  const ogURL = req.originalUrl;
 
-  if (remote == "browser") return res.sendFile(join(__dirname, "index.html"));
-  else if (remote == "origin.rewrite") return rewriteOrigin(req, res, remote);
-  else if (remote != hosts.base && remote != hosts.vercel)
-    url = remote + req.originalUrl;
+  if (remote == host.split(".")[0]) return resolvers.browser(req, res);
+  if (remote == "browser") return resolvers.browser(req, res);
 
-  let headers = req.headers;
-  headers["origin"] && (headers["origin"] = `${req.protocol}://${remote}`);
-  headers["referer"] && (headers["referer"] = `${req.protocol}://${url}`);
-  headers["host"] && delete headers["host"];
+  const fixedOrigin = remote.endsWith("_or");
 
-  process.env.DEBUG && console.log([req.method, url, req.protocol, remote]);
+  const headers = {
+    ...req.headers,
+    origin: fixedOrigin
+      ? parseURL(ogURL.split("/")[0])
+      : `${req.protocol}://${parsedRemote}`,
+    referer: `${req.protocol}://${parsedRemote}${ogURL}`,
+    host: `${req.protocol}://${parsedRemote}`,
+  };
 
-  fetch(`${req.protocol}://${url}`, {
+  fetch(`${req.protocol}://${parsedRemote}${ogURL}`, {
     method: req.method,
     headers,
+    body: req.body || null,
   })
-    .then((res) => res.blob())
+    .then((r) => r.blob())
     .then((body) => {
       res.type(body.type);
-      console.log(url, body.type);
-
-      // rewrite html to use our proxy on any urls
-      if (body.type.split(";")[0] == "text/html") {
-        body.text().then((html) => res.send(rewriteHTML(html, remote, config)));
-      } else {
-        body.arrayBuffer().then((buf) => {
-          res.send(Buffer.from(buf));
-        });
-      }
+      body.arrayBuffer().then((buf) => res.send(Buffer.from(buf)));
     });
 });
-
-module.exports = app;
-
-// app.listen(config.port, () => {
-//   console.clear();
-//   console.log(`[!] Proxy Started at http://localhost:${config.port}`);
-// });
-const rewriteOrigin = (req, res, remote) => {
-  const { rewrite } = config;
-  const url = remote + req.originalUrl;
-
-  const origin = url.split("/")[1];
-  const remoteURL = url.split("/").slice(2).join("/");
-  console.log(url);
-
-  let headers = req.headers;
-  // headers["origin"] = `${req.protocol}://${origin}`;
-  // headers["referer"] = `${req.protocol}://${origin}`;
-  // headers["host"] = `${req.protocol}://${origin}`;
-
-  l("fetching", `${req.protocol}://${remoteURL}`);
-  l("origin", `${req.protocol}://${origin}`);
-  l("headers", headers);
-
-  fetch(`${req.protocol}://${remoteURL}`, {
-    method: req.method,
-    headers,
-  })
-    .then((res) => res.blob())
-    .then((body) => {
-      res.type(body.type);
-      body.arrayBuffer().then((buf) => {
-        res.send(Buffer.from(buf));
-      });
-    });
-};
